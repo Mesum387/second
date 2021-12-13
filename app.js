acaa1d25b1d04197825e94bbdee8174276bc5f3b
Mesum Abbas <syedmesum387@gmail.com>
2021-12-13 14:03:28 +0500

    second

---
+++ app.js
@@ -0,0 +1,81 @@
+//Chapter 21-25
+//Assignment 1
+// var firstname = prompt("Enter Your First Name");
+// var lastname = prompt("Enter Your Last Name");
+// var fullname =  "Welcome " + firstname + " " + lastname + "!";
+// alert(fullname);
+
+
+
+//Assignment 2
+// var favmobile = prompt("Enter Your Favourite Mobile Phone and in last add fullstop(.)");
+// for(var i = 0;i < favmobile.length; i++){
+//     var a = favmobile.slice(i,i + 1)
+//     if(a=="."){
+// alert("Number of strings are: "+ i);}
+//     }  
+
+
+
+
+//Assignment 3
+// var pakistani = "pakistani"
+// alert("String is " + pakistani );
+// for(var i = 0;i < pakistani.length; i++ ){
+//     var noofstring=pakistani.slice(i,i + 1);
+//     if(noofstring=="n"){
+//         alert("Index of 'n'= " + i);
+//     }
+// }
+
+
+
+
+//Assignment 4
+// var a = "Hello World";
+// document.write("String: " + a);
+// for(var i = 0;i<a.length;i++){
+//     var findl = a.slice(i,i + 2);
+//     if (findl=="ld"){
+//         document.write("<br>");
+//         document.write("Last Index of L = "+ i);
+//     }
+// }
+
+
+
+
+
+//Assignment 5
+// var pak = "Pakistani";
+// document.write("String: " + pak);
+// for(var i = 0;i<pak.length;i++){
+// if(i===3){
+//     document.write("<br>")
+//     document.write("Character at index 3 = " + "i")
+//     break;
+    
+// }
+// }
+
+
+
+
+//Assignment 6
+// var firstname = prompt("Enter Your First Name");
+// var lastname = prompt("Enter Your Last Name");
+// var fullname =  "Welcome " + firstname + " " + lastname + "!";
+// alert(fullname);
+
+
+
+//Assignment 7
+// var a = "Hyderabad";
+// document.write("City: " + a);
+// var b = a.replace(/Hyder/g,"Islam");
+// document.write("<br>");
+// document.write("After Replacement: " + b);
+
+
+
+
